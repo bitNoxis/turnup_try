fe1ec10587070a278c9909497a0f850d4a331aab
Lucas Schlatermund <lucas.schlatermund@web.de>
2022-04-22 12:39:07 +0200

    multiple markers on app and gallery

--- lib/screens/map/map.dart
+++ lib/screens/map/map.dart
@@ -1,7 +1,7 @@
-// mapbox Map with markers and info card
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:turnup_try/screens/map/markers.dart';
 
 const MAPBOX_ACCESS_TOKEN =
     'pk.eyJ1IjoibHVjYXNtYXR6ZSIsImEiOiJjbDI4dmtjcHAwYm95M2ptZXM1N3c4dGt3In0._6J67UkB6tn-o_z6quqSkg';
@@ -9,13 +9,44 @@ const MARKER_COLOR = Color(0xFF3DC5A7);
 const URL_TEMPLATE =
     'https://api.mapbox.com/styles/v1/lucasmatze/cl28vr80f000415l1guw9ojlx/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoibHVjYXNtYXR6ZSIsImEiOiJjbDI4dmtjcHAwYm95M2ptZXM1N3c4dGt3In0._6J67UkB6tn-o_z6quqSkg';
 
-final _myLocation = LatLng(53.45, 9.97);
+final _myLocation = LatLng(53.4529399, 9.9733788);
 
-class AnimatedMarkersMap extends StatelessWidget {
+class AnimatedMarkersMap extends StatefulWidget {
   const AnimatedMarkersMap({Key? key}) : super(key: key);
 
   @override
+  _AnimatedMarkersMapState createState() => _AnimatedMarkersMapState();
+}
+
+class _AnimatedMarkersMapState extends State<AnimatedMarkersMap> {
+  final _pageController = PageController();
+
+  List<Marker> _buildMarkers() {
+    final _markerList = <Marker>[];
+    for (int i = 0; i < mapMarkers.length; i++) {
+      final mapItem = mapMarkers[i];
+      _markerList.add(
+        Marker(
+          height: 40,
+          width: 40,
+          point: mapItem.location,
+          builder: (_) {
+            return GestureDetector(
+              onTap: () {
+                print('Selected: ${mapItem.title}');
+              },
+              child: Image.asset('assets/map_marker.png'),
+            );
+          },
+        ),
+      );
+    }
+    return _markerList;
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final _markers = _buildMarkers();
     return Scaffold(
       appBar: AppBar(
         title: Text('Animated Markers'),
@@ -46,21 +77,32 @@ class AnimatedMarkersMap extends StatelessWidget {
                   'id': 'mapbox.mapbox-streets-v8',
                 },
               ),
+              MarkerLayerOptions(
+                markers: _markers,
+              ),
               MarkerLayerOptions(markers: [
                 Marker(
                     point: _myLocation,
                     builder: (_) {
-                      return Container(
-                        height: 50,
-                        width: 50,
-                        decoration: BoxDecoration(
-                          color: MARKER_COLOR,
-                          shape: BoxShape.circle,
-                        ),
-                      );
+                      return _MyLocationMarker();
                     })
               ])
             ],
+          ),
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 20,
+            height: MediaQuery.of(context).size.height * 0.3,
+            child: PageView.builder(
+              itemCount: mapMarkers.length,
+              itemBuilder: (context, index) {
+                final item = mapMarkers[index];
+                return _MapItemDetails(
+                  mapMarker: item,
+                );
+              },
+            ),
           ),
         ],
       ),
@@ -77,8 +119,67 @@ class _MyLocationMarker extends StatelessWidget {
         height: 50,
         width: 50,
         decoration: BoxDecoration(
-          color: Color.fromARGB(255, 44, 170, 107),
+          color: MARKER_COLOR,
           shape: BoxShape.circle,
+        ));
+  }
+}
+
+class _MapItemDetails extends StatelessWidget {
+  const _MapItemDetails({
+    Key? Key,
+    required this.mapMarker,
+  }) : super(key: Key);
+
+  final MapMarker mapMarker;
+
+  @override
+  Widget build(BuildContext context) {
+    final _styleTitle = TextStyle(
+        color: Colors.black, fontSize: 18, fontWeight: FontWeight.bold);
+    final _styleAddress = TextStyle(color: Colors.grey[800], fontSize: 14);
+    return Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Card(
+          margin: EdgeInsets.zero,
+          color: Colors.white,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Expanded(
+                child: Row(children: [
+                  Expanded(
+                    child: Image.asset(mapMarker.image),
+                  ),
+                  Expanded(
+                      child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        mapMarker.title,
+                        style: _styleTitle,
+                      ),
+                      const SizedBox(height: 10),
+                      Text(
+                        mapMarker.address,
+                        style: _styleAddress,
+                      ),
+                    ],
+                  ))
+                ]),
+              ),
+              MaterialButton(
+                padding: EdgeInsets.zero,
+                onPressed: () => null,
+                color: MARKER_COLOR,
+                elevation: 6,
+                child: Text(
+                  'Call',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ),
+            ],
+          ),
         ));
   }
 }
---
+++ lib/screens/map/markers.dart
@@ -0,0 +1,57 @@
+import 'package:latlong2/latlong.dart';
+
+class MapMarker {
+  const MapMarker({
+    required this.image,
+    required this.title,
+    required this.address,
+    required this.location,
+  });
+  final String image;
+  final String title;
+  final String address;
+  final LatLng location;
+}
+
+final _locations = [
+  LatLng(53.4452934, 9.9608823), //Josias
+  LatLng(53.4673132, 9.9853174), //Yachtclub Hansa Harburg
+  LatLng(53.4548382, 9.9359941), //Maxi
+  LatLng(53.4667889, 9.9566378), //Pfaffe
+  LatLng(53.4794747, 9.8914109), //Björn
+];
+
+const _path = 'assets/';
+
+final mapMarkers = [
+  MapMarker(
+    image: '${_path}logo.png',
+    title: 'Josias',
+    address: 'Am großen Dahlen 30',
+    location: _locations[0],
+  ),
+  MapMarker(
+    image: '${_path}logo.png',
+    title: 'Yachtclub Hansa Harburg',
+    address: 'zitadellenstraße 10',
+    location: _locations[1],
+  ),
+  MapMarker(
+    image: '${_path}logo.png',
+    title: 'Maxi',
+    address: 'Ehesdorfer Weg 144',
+    location: _locations[2],
+  ),
+  MapMarker(
+    image: '${_path}logo.png',
+    title: 'Pfaffe',
+    address: 'Gildering 17',
+    location: _locations[3],
+  ),
+  MapMarker(
+    image: '${_path}logo.png',
+    title: 'Bjarne',
+    address: 'Ünner Brandheid 22A',
+    location: _locations[4],
+  ),
+];
