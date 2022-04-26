c6d644ec39a881c74fc0b035648233560cb318c3
Lucas Schlatermund <lucas.schlatermund@web.de>
2022-04-27 00:07:41 +0200

    navigation thru tap on pinpoint

--- lib/screens/map/map.dart
+++ lib/screens/map/map.dart
@@ -1,3 +1,6 @@
+import 'dart:ffi';
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
@@ -9,6 +12,9 @@ const MARKER_COLOR = Color(0xFF3DC5A7);
 const URL_TEMPLATE =
     'https://api.mapbox.com/styles/v1/lucasmatze/cl28vr80f000415l1guw9ojlx/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoibHVjYXNtYXR6ZSIsImEiOiJjbDI4dmtjcHAwYm95M2ptZXM1N3c4dGt3In0._6J67UkB6tn-o_z6quqSkg';
 
+const MARKER_SIZE_EXPANDED = 55.0;
+const MARKER_SIZE_SHRINKED = 35.0;
+
 final _myLocation = LatLng(53.4529399, 9.9733788);
 
 class AnimatedMarkersMap extends StatefulWidget {
@@ -18,8 +24,11 @@ class AnimatedMarkersMap extends StatefulWidget {
   _AnimatedMarkersMapState createState() => _AnimatedMarkersMapState();
 }
 
-class _AnimatedMarkersMapState extends State<AnimatedMarkersMap> {
+class _AnimatedMarkersMapState extends State<AnimatedMarkersMap>
+    with SingleTickerProviderStateMixin {
   final _pageController = PageController();
+  late final AnimationController _animationController;
+  int _selectedIndex = 0;
 
   List<Marker> _buildMarkers() {
     final _markerList = <Marker>[];
@@ -27,21 +36,43 @@ class _AnimatedMarkersMapState extends State<AnimatedMarkersMap> {
       final mapItem = mapMarkers[i];
       _markerList.add(
         Marker(
-          height: 40,
-          width: 40,
+          height: MARKER_SIZE_EXPANDED,
+          width: MARKER_SIZE_EXPANDED,
           point: mapItem.location,
           builder: (_) {
             return GestureDetector(
               onTap: () {
-                print('Selected: ${mapItem.title}');
+                _selectedIndex = i;
+                setState(() {
+                  _pageController.animateToPage(i,
+                      duration: const Duration(milliseconds: 500),
+                      curve: Curves.fastLinearToSlowEaseIn);
+                  print('Selected: ${mapItem.title}');
+                });
               },
-              child: Image.asset('assets/mapmarker.png'),
+              child: _LocationMarker(
+                selected: _selectedIndex == i,
+              ),
             );
           },
         ),
       );
     }
     return _markerList;
+  }
+
+  @override
+  void initState() {
+    _animationController = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 600));
+    _animationController.repeat(reverse: true);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
   }
 
   @override
@@ -82,9 +113,11 @@ class _AnimatedMarkersMapState extends State<AnimatedMarkersMap> {
               ),
               MarkerLayerOptions(markers: [
                 Marker(
+                    height: 60,
+                    width: 60,
                     point: _myLocation,
                     builder: (_) {
-                      return _MyLocationMarker();
+                      return _MyLocationMarker(_animationController);
                     })
               ])
             ],
@@ -95,6 +128,8 @@ class _AnimatedMarkersMapState extends State<AnimatedMarkersMap> {
             bottom: 20,
             height: MediaQuery.of(context).size.height * 0.3,
             child: PageView.builder(
+              controller: _pageController,
+              physics: const NeverScrollableScrollPhysics(),
               itemCount: mapMarkers.length,
               itemBuilder: (context, index) {
                 final item = mapMarkers[index];
@@ -110,18 +145,61 @@ class _AnimatedMarkersMapState extends State<AnimatedMarkersMap> {
   }
 }
 
-class _MyLocationMarker extends StatelessWidget {
-  const _MyLocationMarker({Key? Key}) : super(key: Key);
+class _LocationMarker extends StatelessWidget {
+  const _LocationMarker({Key? key, this.selected = false}) : super(key: key);
+
+  final bool selected;
+  @override
+  Widget build(BuildContext context) {
+    final size = selected ? MARKER_SIZE_EXPANDED : MARKER_SIZE_SHRINKED;
+    return Center(
+      child: AnimatedContainer(
+        height: size,
+        width: size,
+        duration: const Duration(milliseconds: 400),
+        child: Image.asset('assets/mapmarker.png'),
+      ),
+    );
+  }
+}
+
+class _MyLocationMarker extends AnimatedWidget {
+  const _MyLocationMarker(Animation<double> animation, {Key? Key})
+      : super(
+          key: Key,
+          listenable: animation,
+        );
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        height: 50,
-        width: 50,
-        decoration: BoxDecoration(
-          color: MARKER_COLOR,
-          shape: BoxShape.circle,
-        ));
+    final value = (listenable as Animation<double>).value;
+    final newValue = lerpDouble(0.5, 1.0, value)!;
+    final size = 50.0;
+    return Center(
+        child: Stack(
+      children: [
+        Center(
+          child: Container(
+            height: size * newValue,
+            width: size * newValue,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: MARKER_COLOR.withOpacity(0.5),
+            ),
+          ),
+        ),
+        Center(
+          child: Container(
+            height: 20,
+            width: 20,
+            decoration: BoxDecoration(
+              color: MARKER_COLOR,
+              shape: BoxShape.circle,
+            ),
+          ),
+        ),
+      ],
+    ));
   }
 }
 
@@ -147,7 +225,7 @@ class _MapItemDetails extends StatelessWidget {
             borderRadius: BorderRadius.circular(20.0),
             boxShadow: const [
               BoxShadow(
-                color: Colors.grey,
+                color: Colors.black,
                 blurRadius: 10.0,
                 spreadRadius: 1.0,
                 offset: Offset(4.0, 4.0),
